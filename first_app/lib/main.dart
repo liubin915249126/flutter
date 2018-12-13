16ed62675153e756c52766e98293af6ddb8d5257
liubin915249126 <915249126@qq.com>
2018-12-13 13:48:48 +0800

    modify color

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,6 +6,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          //child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
